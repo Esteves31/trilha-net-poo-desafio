4fa611dff38a59dbde09711f622e5610877f51b3
Matheus <matheus.resteves@gmail.com>
2024-09-16 14:24:08 -0300

    Adicionando testes de herança

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia cel1 = new Nokia("12345678", "Tijolão", "sla", 64);
+Iphone cel2 = new Iphone("12345678", "15", "sla", 256);
+
+cel1.ReceberLigacao();
+cel1.Ligar();
+cel1.InstalarAplicativo("Clash of Clans");
+
+cel2.ReceberLigacao();
+cel2.Ligar();
+cel2.InstalarAplicativo("Clash of Clans");
